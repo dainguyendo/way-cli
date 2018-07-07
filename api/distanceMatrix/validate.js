a2ccde85633f9cf0c2afee6b6c27944a1929a037
dainguyendo <dainguyendo@gmail.com>
2018-07-07 11:58:28 -0400

    Fixed validation for origins and destinations for distance matrix api

--- api/distanceMatrix/validate.js
+++ api/distanceMatrix/validate.js
@@ -2,8 +2,8 @@ const validations = require('../../validations');
 
 module.exports = (origins, destinations, mode, unit, avoid) => {
     const errors = [];
-    if (!validations.validateString(origins)) {errors.push('No origins defined.') }
-    if (!validations.validateString(destinations)) { errors.push('No destinations defined.') }
+    if (origins.map(orig => validations.validateString(orig)).some(orig => !orig)) {errors.push('No origins defined.') }
+    if (destinations.map(dest => validations.validateString(dest)).some(dest => !dest)) { errors.push('No destinations defined.') }
     if (!validations.validateMode(mode)) { errors.push('Invalid mode argument.') }
     if (!validations.validateUnits(unit)) { errors.push('Invalid unit argument.') }
     if (!validations.validateAvoids(avoid)) { errors.push('Invalid avoid argument.') }
