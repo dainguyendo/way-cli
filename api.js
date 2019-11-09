48b1d1ed7b56ddd269c90a275fa3781d9d699a72
Dai Nguyendo <dainguyendo@gmail.com>
2019-11-09 16:14:45 -0500

    deleting old files

--- api.js
+++
@@ -1,9 +0,0 @@
-const handleDistanceMatrixAPI = require('./api/distanceMatrix/handler');
-const handleDirectionsAPI = require('./api/directions/handler');
-const handleGetAliasAPI = require('./api/aliases/get');
-
-module.exports = {
-    distanceMatrix: handleDistanceMatrixAPI,
-    directions: handleDirectionsAPI,
-    getAlias: handleGetAliasAPI
-};
